c53b0117bd60b0d4899bad723e8728e446d36753
Njpatman <Njpatman@users.noreply.github.com>
2024-01-24 23:28:02 -0500

    Fixed faction issue

--- scripts/Game/Systems/COA_SafeStart.c
+++ scripts/Game/Systems/COA_SafeStart.c
@@ -21,12 +21,19 @@ class CRF_TNK_SafestartComponent: SCR_BaseGameModeComponent
 	[RplProp()]
 	protected bool m_bIndforReady = false;
 	
+	// We use every m_sServerWorldTime update to check all players EHs. (NOTE: this only updates when the string updates, so even though the loop m_sServerWorldTime refreshes every 250ms in practice it's every second)
+	[RplProp(onRplName: "ToggleSafeStartEHs")]
+	protected string m_sServerWorldTime;
+	
 	[RplProp(onRplName: "ShowMessage")]
 	protected string m_sMessageContent = "";
 	
 	protected int m_iSafeStartTimeRemaining = 35;
 	
-	private ref array<IEntity> m_aControlledEntityArray = new array<IEntity>;
+	protected int playedFactions = 0;
+	protected bool m_bBluforExists = false;
+	protected bool m_bOpforExists = false;
+	protected bool m_bIndforExists = false;
 	
 	//------------------------------------------------------------------------------------------------
 
@@ -61,37 +68,6 @@ class CRF_TNK_SafestartComponent: SCR_BaseGameModeComponent
 		} 
 	}
 	
-	override void OnPlayerSpawned(int playerId, IEntity controlledEntity)
-	{	
-		super.OnPlayerSpawned(playerId, controlledEntity);
-		//Print("[CRF Safestart] Enabling safestart local");
-		if (!Replication.IsServer()) return;
-		m_aControlledEntityArray.Insert(controlledEntity);
-
-		EventHandlerManagerComponent m_eventHandler = EventHandlerManagerComponent.Cast(controlledEntity.FindComponent(EventHandlerManagerComponent));
-		if (!m_eventHandler) return;
-		
-		CharacterControllerComponent charComp = CharacterControllerComponent.Cast(controlledEntity.FindComponent(CharacterControllerComponent));
-		if (!charComp) return;
-		
-		if (GetSafestartStatus()) {
-			// Set safe/lowered
-			charComp.SetSafety(true, true);
-			//Print("[CRF Safestart] Adding EHs");
-			
-			// Add EH to prevent bullets from being fired
-			m_eventHandler.RegisterScriptHandler("OnProjectileShot", this, OnWeaponFired);
-			m_eventHandler.RegisterScriptHandler("OnGrenadeThrown", this, OnGrenadeThrown);	
-		} else {
-			charComp.SetSafety(false, false);
-			//Print("[CRF Safestart] Removing EHs");
-			
-			// Remove EH
-			m_eventHandler.RemoveScriptHandler("OnProjectileShot", this, OnWeaponFired);
-			m_eventHandler.RemoveScriptHandler("OnGrenadeThrown", this, OnGrenadeThrown);
-		};
-	}
-	
 	//------------------------------------------------------------------------------------------------
 
 	// Ready Up functions
@@ -104,12 +80,48 @@ class CRF_TNK_SafestartComponent: SCR_BaseGameModeComponent
 	}
 	
 	//Call from server
-	void ToggleSideReady(string setReady) {
+	void UpdateServerWorldTime()
+	{
+		float currentTime = GetGame().GetWorld().GetWorldTime();
+		float millis = m_iTimeSafeStartBegan - currentTime;
+		
+  		int totalSeconds = (millis / 1000);
+		
+		m_sServerWorldTime = SCR_FormatHelper.FormatTime(totalSeconds);
+		
+		// We also use this loop to check all players EHs
+		ToggleSafeStartEHs();
+		
+		Replication.BumpMe();
+	};
+	
+	//Call from server
+	void ToggleSideReady(string setReady, string playerName) {
+		if (!GetSafestartStatus()) return;
+		
 		switch (setReady)
 		{
-			case("Blufor") : {m_bBluforReady = !m_bBluforReady; break;};
-			case("Opfor")  : {m_bOpforReady = !m_bOpforReady;   break;};
-			case("Indfor") : {m_bIndforReady = !m_bIndforReady; break;};
+			case("Blufor") : {
+				m_bBluforReady = !m_bBluforReady; 
+				if (m_bBluforReady) m_sMessageContent = string.Format("[CRF] : %1 Has Readied Up Blufor", playerName);
+				if (!m_bBluforReady) m_sMessageContent = string.Format("[CRF] : %1 Has Unreadied Up Blufor", playerName);
+				if (!m_bBluforExists) {m_bBluforExists = true; playedFactions = playedFactions + 1;};
+				break;
+			};
+			case("Opfor")  : {
+				m_bOpforReady = !m_bOpforReady;
+				if (m_bOpforReady) m_sMessageContent = string.Format("[CRF] : %1 Has Readied Up Opfor", playerName);
+				if (!m_bOpforReady) m_sMessageContent = string.Format("[CRF] : %1 Has Unreadied Up Opfor", playerName);
+				if (!m_bOpforExists) {m_bOpforExists = true; playedFactions = playedFactions + 1;};
+				break;
+			};
+			case("Indfor") : {
+				m_bIndforReady = !m_bIndforReady; 
+				if (m_bIndforReady) m_sMessageContent = string.Format("[CRF] : %1 Has Readied Up Indfor", playerName);
+				if (!m_bIndforReady) m_sMessageContent = string.Format("[CRF] : %1 Has Unreadied Up Indfor", playerName);
+				if (!m_bIndforExists) {m_bIndforExists = true; playedFactions = playedFactions + 1;};
+				break;
+			};
 		};
 		Replication.BumpMe();
 	}
@@ -117,22 +129,6 @@ class CRF_TNK_SafestartComponent: SCR_BaseGameModeComponent
 	//Call from server
 	protected void CheckStartCountDown()
 	{		
-	 	SCR_FactionManager factionManager = SCR_FactionManager.Cast(GetGame().GetFactionManager());
-		
-		SCR_SortedArray<SCR_Faction> outFaction = new SCR_SortedArray<SCR_Faction>;
-		factionManager.GetSortedFactionsList(outFaction);
-		
-		if (!outFaction || outFaction.IsEmpty()) return;
-		
-		array<SCR_Faction> outArray = new array<SCR_Faction>;
-		outFaction.ToArray(outArray);
-		
-		int playedFactions = 0;
-		foreach(SCR_Faction faction : outArray) {
-			if (faction.GetPlayerCount() == 0) continue;
-			playedFactions = playedFactions + 1;
-		};
-		
 		array<bool> factionsReady = {m_bBluforReady, m_bOpforReady, m_bIndforReady};
 		
 		int factionsReadyCount = 0;
@@ -152,7 +148,7 @@ class CRF_TNK_SafestartComponent: SCR_BaseGameModeComponent
 		
 		if (factionsReadyCount == playedFactions) {
 			m_iSafeStartTimeRemaining = m_iSafeStartTimeRemaining - 5;
-			m_sMessageContent = string.Format("[CRF] : Game Live In: %1", m_iSafeStartTimeRemaining);
+			m_sMessageContent = string.Format("[CRF] : Game Live In: %1 Seconds!", m_iSafeStartTimeRemaining);
 			if (m_iSafeStartTimeRemaining == 0) {
 				ToggleSafeStartServer(false);
 				m_sMessageContent = string.Format("[CRF] : GAME LIVE!", m_iSafeStartTimeRemaining);
@@ -161,6 +157,7 @@ class CRF_TNK_SafestartComponent: SCR_BaseGameModeComponent
 		Replication.BumpMe();
 	};
 	
+	//Called from server to all clients
 	void ShowMessage()
 	{
 		PlayerController pc = GetGame().GetPlayerController();
@@ -178,69 +175,9 @@ class CRF_TNK_SafestartComponent: SCR_BaseGameModeComponent
 
 	//------------------------------------------------------------------------------------------------
 	
-	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
-	protected void ToggleSafeStartServer(bool status) 
+	string GetServerWorldTime()
 	{
-		//Print("[CRF Safestart] Enabling safestart server");
-		
-		if (status) // Turn on safestart
-		{
-			if (m_SafeStartEnabled)
-			{
-				//Print("[CRF Safestart] Safestart already enabled. Exiting.");
-				return;
-			}
-			
-			m_iTimeSafeStartBegan = GetGame().GetWorld().GetWorldTime();
-			m_SafeStartEnabled = true;
-			m_iSafeStartTimeRemaining = 35;
-			GetGame().GetCallqueue().CallLater(CheckStartCountDown, 5000, true);
-			ToggleSafeStartEHs();
-		} else { // Turn off safestart
-			if (!m_SafeStartEnabled) 
-			{
-				//Print("[CRF Safestart] Safestart already disabled. Exiting.");
-				return;
-			}		
-			
-			m_SafeStartEnabled = false;
-			m_bBluforReady = false;
-			m_bOpforReady = false;
-			m_bIndforReady = false;
-			GetGame().GetCallqueue().Remove(CheckStartCountDown);
-			ToggleSafeStartEHs();
-		}
-		
-		Replication.BumpMe(); //Broadcast m_SafeStartEnabled change
-	};
-	
-	protected void ToggleSafeStartEHs()
-	{	
-		foreach (IEntity controlledEntity : m_aControlledEntityArray) 
-		{
-			EventHandlerManagerComponent m_eventHandler = EventHandlerManagerComponent.Cast(controlledEntity.FindComponent(EventHandlerManagerComponent));
-			if (!m_eventHandler) return;
-		
-			CharacterControllerComponent charComp = CharacterControllerComponent.Cast(controlledEntity.FindComponent(CharacterControllerComponent));
-			if (!charComp) return;
-		
-			if (GetSafestartStatus()) {
-				// Set safe/lowered
-				charComp.SetSafety(true, true);
-				//Print("[CRF Safestart] Adding EHs");
-			
-				// Add EH to prevent bullets from being fired
-				m_eventHandler.RegisterScriptHandler("OnProjectileShot", this, OnWeaponFired);
-				m_eventHandler.RegisterScriptHandler("OnGrenadeThrown", this, OnGrenadeThrown);	
-			} else {
-				charComp.SetSafety(false, false);
-				//Print("[CRF Safestart] Removing EHs");
-			
-				// Remove EH
-				m_eventHandler.RemoveScriptHandler("OnProjectileShot", this, OnWeaponFired);
-				m_eventHandler.RemoveScriptHandler("OnGrenadeThrown", this, OnGrenadeThrown);
-			};
-		};
+		return m_sServerWorldTime;
 	}
 	
 	bool GetSafestartStatus()
@@ -248,25 +185,97 @@ class CRF_TNK_SafestartComponent: SCR_BaseGameModeComponent
 		return m_SafeStartEnabled;
 	}
 	
-	int GetTimeSafeStartBegan()
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	protected void ToggleSafeStartServer(bool status) 
 	{
-		return m_iTimeSafeStartBegan;
+		if (status) { // Turn on safestart
+			if (m_SafeStartEnabled) return;
+			
+			m_iTimeSafeStartBegan = GetGame().GetWorld().GetWorldTime();
+			m_SafeStartEnabled = true;
+			m_iSafeStartTimeRemaining = 35;
+			
+			GetGame().GetCallqueue().CallLater(CheckStartCountDown, 5000, true);
+			GetGame().GetCallqueue().CallLater(UpdateServerWorldTime, 250, true);
+			
+			Replication.BumpMe();//Broadcast m_SafeStartEnabled change
+			
+		} else { // Turn off safestart
+			if (!m_SafeStartEnabled) return;	
+			
+			m_SafeStartEnabled = false;
+			m_bBluforReady = false;
+			m_bOpforReady = false;
+			m_bIndforReady = false;
+			
+			GetGame().GetCallqueue().Remove(CheckStartCountDown);
+			GetGame().GetCallqueue().Remove(UpdateServerWorldTime);
+			
+			Replication.BumpMe();//Broadcast m_SafeStartEnabled change
+			
+			// Use UpdateServerWorldTime to essentially delay the call for the removal of EHs so the changes to m_SafeStartEnabled can propagate.
+			GetGame().GetCallqueue().CallLater(UpdateServerWorldTime, 2850);
+			
+			// Even longer delay just in case there's any edge cases we didnt anticipate.
+			GetGame().GetCallqueue().CallLater(UpdateServerWorldTime, 12500);
+		}
+	};
+	
+	protected void ToggleSafeStartEHs()
+	{	
+		array<int> outPlayers = {};
+		GetGame().GetPlayerManager().GetPlayers(outPlayers);
+		
+		array<string> eventHandlerStrings = {"OnProjectileShot", "OnGrenadeThrown"};
+		
+		foreach (int playerID : outPlayers)
+		{
+			IEntity controlledEntity = GetGame().GetPlayerManager().GetPlayerControlledEntity(playerID);
+			if (!controlledEntity) continue;
+			
+			EventHandlerManagerComponent m_eventHandler = EventHandlerManagerComponent.Cast(controlledEntity.FindComponent(EventHandlerManagerComponent));
+			if (!m_eventHandler) continue;
+		
+			array<BaseEventHandler> outEventHandlers = {};
+			m_eventHandler.GetEventHandlers(outEventHandlers);
+		
+			CharacterControllerComponent charComp = CharacterControllerComponent.Cast(controlledEntity.FindComponent(CharacterControllerComponent));
+			if (!charComp) continue;
+			
+			bool alreadyHasEventHandlers = false;
+			foreach (BaseEventHandler eventHandlers : outEventHandlers) {
+				string eventHandlerName = eventHandlers.GetEventName();
+				if (eventHandlerStrings.Contains(eventHandlerName)) {
+					alreadyHasEventHandlers = true;
+					continue;
+				};
+			};
+		
+			if (!alreadyHasEventHandlers && GetSafestartStatus()) {
+				charComp.SetSafety(true, true);
+				m_eventHandler.RegisterScriptHandler("OnProjectileShot", this, OnWeaponFired);
+				m_eventHandler.RegisterScriptHandler("OnGrenadeThrown", this, OnGrenadeThrown);
+			};
+		
+			if (alreadyHasEventHandlers && !GetSafestartStatus()) {
+				charComp.SetSafety(false, false);
+				m_eventHandler.RemoveScriptHandler("OnProjectileShot", this, OnWeaponFired);
+				m_eventHandler.RemoveScriptHandler("OnGrenadeThrown", this, OnGrenadeThrown);
+			};
+		};
 	}
 	
 	protected void OnWeaponFired(int playerID, BaseWeaponComponent weapon, IEntity entity)
 	{		
-		//Print("[CRF Safestart] Gun shot");
-		
 		// Get projectile and delete it
 		delete entity;
 	}
 	
 	protected void OnGrenadeThrown(int playerID, BaseWeaponComponent weapon, IEntity entity)
 	{
-		//Print("[CRF Safestart] Grenade thrown");
 		if (!weapon)
 			return;
-
+		
 		// Get grenade and delete it
 		delete entity;
 	}
