e47b81a2407d558022f2d5d2eb35ae08cb91f744
Salami <tylersiladi2003@gmail.com>
2024-09-03 16:21:04 -0400

    Should be finished tm

--- scripts/Game/Systems/Respawns/RadioRespawnGameComponent.c
+++ scripts/Game/Systems/Respawns/RadioRespawnGameComponent.c
@@ -7,14 +7,33 @@ class CRF_RadioRespawnSystemComponentClass: SCR_BaseGameModeComponentClass
 class CRF_RadioRespawnSystemComponent: SCR_BaseGameModeComponent
 {
 
-	[Attribute(desc: "Prefabs for Blufor", category: "Blufor Respawns")];
-	protected ref array<string> m_bluforSpawnPoints;
+	[Attribute("respawn", "auto", "BLUFOR Respawn Point")];
+	protected string m_bluforSpawnPoint;
+	
+	[Attribute("respawn", "auto", "OFPOR Respawn Point")];
+	protected string m_opforSpawnPoint;
+	
+	[Attribute("respawn", "auto", "INDFOR Respawn Point")];
+	protected string m_indforSpawnPoint;
+	
+	[Attribute("1", "auto", "Amount of times SLs can call in reinforcements.")];
+	protected int m_respawnWaves;
+	
+	[RplProp(onRplName: "SpawnPrefabs")]
+	string m_tempPrefab;
+	ref EntitySpawnParams bluforspawnParams = new EntitySpawnParams();
+	int m_groupID;
+	int m_tempPlayerID;
+	
+	IEntity tempEnt;
 	
 	protected SCR_GroupsManagerComponent m_GroupsManagerComponent;
-	protected ref map<int, int> m_entitySlots = new map<int, int>();
-	protected ref map<int, string> m_entityPrefabs = new map<int, string>();
 	CRF_SafestartGameModeComponent m_safestart;
 	protected ref array<int> m_respawnedGroups;
+	
+	protected ref map<IEntity, int> m_entitySlots = new map<IEntity, int>();
+	protected ref map<IEntity, string> m_entityPrefabs = new map<IEntity, string>();
+	protected ref map<IEntity, int> m_entityID = new map<IEntity, int>();
 	
 	override protected void OnPostInit(IEntity owner)
 	{
@@ -54,12 +73,9 @@ class CRF_RadioRespawnSystemComponent: SCR_BaseGameModeComponent
 			{
 				IEntity controlledEntity = GetGame().GetPlayerManager().GetPlayerControlledEntity(playerID);			
 				string prefabName = controlledEntity.GetPrefabData().GetPrefabName();
-				m_entitySlots.Insert(1, groupID);
-				m_entitySlots.Insert(2, groupID);
-				m_entitySlots.Insert(3, 1);
-				m_entityPrefabs.Insert(1, prefabName);
-				m_entityPrefabs.Insert(2, prefabName);
-				m_entityPrefabs.Insert(3, prefabName);
+				m_entitySlots.Insert(controlledEntity, groupID);
+				m_entityPrefabs.Insert(controlledEntity, prefabName);
+				m_entityID.Insert(controlledEntity, playerID);
 			}
 		}
 	}
@@ -88,21 +104,104 @@ class CRF_RadioRespawnSystemComponent: SCR_BaseGameModeComponent
 	
 	void SpawnGroup(int groupID)
 	{
-		RPC_SpawnGroup(groupID);
 		Rpc(RPC_SpawnGroup, groupID);
 	}
 	
-	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
 	void RPC_SpawnGroup(int groupID)
 	{
+		m_respawnedGroups.Insert(groupID);
+		int timesRespawned;
+		foreach (int wave : m_respawnedGroups)
+		{
+			if(wave == groupID)
+			{
+				timesRespawned++;
+			}
+		}
+		
+		if(timesRespawned >= m_respawnWaves)
+			return;
+		
 		for(int i, count = m_entitySlots.Count(); i < count; ++i)
 		{
-			int tempEntity = m_entitySlots.GetKeyByValue(groupID);
+			m_groupID = groupID;
+			Replication.BumpMe();
+			IEntity tempEntity = m_entitySlots.GetKeyByValue(groupID);
+			m_tempPlayerID = m_entityID.Get(tempEntity);
+			Replication.BumpMe();
+			
 			if (!tempEntity)
 				return;
 			
-			Print("hello");
+			if(SCR_AIDamageHandling.IsAlive(tempEntity))
+				return;
+			
+			Faction faction = SCR_FactionManager.SGetPlayerFaction(m_tempPlayerID);
+			Color factionColor = faction.GetFactionColor();
+			float rg = Math.Max(factionColor.R(), factionColor.G());
+			float rgb = Math.Max(rg, factionColor.B());
+			
+			switch (rgb)
+			{
+				case (rgb == factionColor.B()):
+					GetSpawnParamsByFaction(0);
+					break;
+				
+				case (rgb == factionColor.R()):
+					GetSpawnParamsByFaction(1);
+					break;
+				
+				case (rgb == factionColor.G()):
+					GetSpawnParamsByFaction(2);
+					break;
+			}
+			
+			
+			string m_tempPrefab = m_entityPrefabs.Get(tempEntity);
+			Replication.BumpMe();
+			
+			SpawnPrefabs();
 			m_entitySlots.Remove(tempEntity);
+			m_entityPrefabs.Remove(tempEntity);
+			m_entityID.Remove(tempEntity);
+			m_entitySlots.Insert(tempEnt, groupID);
+			m_entityPrefabs.Insert(tempEnt, m_tempPrefab);
+			m_entityID.Insert(tempEnt, m_tempPlayerID);
+		}
+	}
+	
+	void SpawnPrefabs()
+	{
+		IEntity tempEnt = GetGame().SpawnEntityPrefab(Resource.Load(m_tempPrefab),GetGame().GetWorld(),bluforspawnParams);
+		m_GroupsManagerComponent.AddPlayerToGroup(m_groupID, m_tempPlayerID);
+	}
+	
+	//0 = BLUFOR
+	//1 = OPFOR
+	//2 = INDFOR
+	void GetSpawnParamsByFaction(int factionNumber)
+	{
+		if (factionNumber == 0)
+		{
+			protected vector bluforSpawn = GetGame().GetWorld().FindEntityByName(m_bluforSpawnPoint).GetOrigin();
+        	bluforspawnParams.TransformMode = ETransformMode.WORLD;
+        	bluforspawnParams.Transform[3] = bluforSpawn;
+			Replication.BumpMe();
+		}
+		if (factionNumber == 1)
+		{
+			protected vector spawnPoint = GetGame().GetWorld().FindEntityByName(m_opforSpawnPoint).GetOrigin();
+        	bluforspawnParams.TransformMode = ETransformMode.WORLD;
+        	bluforspawnParams.Transform[3] = spawnPoint;
+			Replication.BumpMe();
+		}
+		if (factionNumber == 2)
+		{
+			protected vector spawnPoint = GetGame().GetWorld().FindEntityByName(m_indforSpawnPoint).GetOrigin();
+        	bluforspawnParams.TransformMode = ETransformMode.WORLD;
+        	bluforspawnParams.Transform[3] = spawnPoint;
+			Replication.BumpMe();
 		}
 	}
 }
