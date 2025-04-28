20d747ad2fc44ab464189f17f2dd6bf1350310d6
Troy <tanaka_s2@yahoo.com>
2025-04-28 13:33:54 -0500

    Comment map marker manager

--- scripts/Game/Systems/VanillaOverrides/Managers/CRF_SCR_MapMarkerManagerComponent.c
+++ scripts/Game/Systems/VanillaOverrides/Managers/CRF_SCR_MapMarkerManagerComponent.c
@@ -1,3 +1,4 @@
+// Prevents map markers from being deleted when a player disconnects
 modded class SCR_MapMarkerManagerComponent
 {
 	override void OnPlayerDisconnected(int playerId, KickCauseCode cause, int timeout)
