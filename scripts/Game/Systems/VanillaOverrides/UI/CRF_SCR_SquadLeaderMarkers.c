22e339a7d1bb892fa066bef7193ffb18e1938d07
Troy <tanaka_s2@yahoo.com>
2025-04-28 15:47:49 -0500

    Comment squad lead markers

--- scripts/Game/Systems/VanillaOverrides/UI/CRF_SCR_SquadLeaderMarkers.c
+++ scripts/Game/Systems/VanillaOverrides/UI/CRF_SCR_SquadLeaderMarkers.c
@@ -1,11 +1,19 @@
 modded class SCR_MapMarkerSquadLeader
 {
+	/**
+	 * Override method called when player ID is updated.
+	 * Makes the squad leader marker visible to the local player.
+	 */
 	override void OnPlayerIdUpdate()
 	{
+		// Get the reference to the player controller from the game
 		PlayerController pController = GetGame().GetPlayerController();
+		
+		// If no valid player controller is found, exit the method
 		if (!pController)
 			return;
 		
+		// Set this squad leader marker to be visible to the local player
 		SetLocalVisible(true);
 	}
 }
