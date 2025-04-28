b78dc0b98679be1c57090ce4d560c0db3ed3062f
Troy <tanaka_s2@yahoo.com>
2025-04-28 13:16:28 -0500

    Stamina clean up

--- scripts/Game/Systems/VanillaOverrides/Character/CRF_SCR_CharacterStaminaComponent.c
+++ scripts/Game/Systems/VanillaOverrides/Character/CRF_SCR_CharacterStaminaComponent.c
@@ -1,13 +1,23 @@
 modded class SCR_CharacterStaminaComponent : CharacterStaminaComponent
 {
+	//-------------------------------------------------------------------------
+	// Override: Prevents stamina drain during safestart mode
+	//-------------------------------------------------------------------------
 	override event void OnStaminaDrain(float pDrain)
 	{
+		// Get the Safestart Manager instance
 		CRF_SafestartManager SafestartManager = CRF_SafestartManager.GetInstance();
 		
-		if(!SafestartManager)
+		// If Safestart Manager doesn't exist, exit early
+		if (!SafestartManager)
 			return;
 		
-		if(GetGame().InPlayMode() && SafestartManager.GetSafestartStatus())
-			AddStamina(Math.AbsFloat(pDrain));
+		// If in play mode and safestart is active, prevent stamina drain
+		// by adding back the same amount that was drained
+		if (GetGame().InPlayMode() && SafestartManager.GetSafestartStatus())
+		{
+			float staminaToRestore = Math.AbsFloat(pDrain);
+			AddStamina(staminaToRestore);
+		}
 	};
 }
