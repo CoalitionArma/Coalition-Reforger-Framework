475c1d0e1f73073675b709764c5209af1cd8ba02
Troy <tanaka_s2@yahoo.com>
2025-04-28 13:43:11 -0500

    Comment SCR_NotificationSenderComponent

--- scripts/Game/Systems/VanillaOverrides/UI/CRF_SCR_NotificationSenderComponent.c
+++ scripts/Game/Systems/VanillaOverrides/UI/CRF_SCR_NotificationSenderComponent.c
@@ -1,13 +1,25 @@
 modded class SCR_NotificationSenderComponent
 {
+	//----------------------------------------------------------------
+	// Sets the kill feed to display full information
+	// Configures the local player to receive all kill notifications
+	//----------------------------------------------------------------
 	void SetKillFeedTypeDeadLocal()
 	{
+		// Set kill feed to show complete information
 		m_iKillFeedType = EKillFeedType.FULL;
+		
+		// Configure to receive all types of kill feed notifications
 		m_iReceiveKillFeedType = EKillFeedReceiveType.ALL;
 	}
 	
+	//----------------------------------------------------------------
+	// Disables all kill feed notifications for the local player
+	// Used when kill feed display should be turned off completely
+	//----------------------------------------------------------------
 	void SetKillFeedTypeNoneLocal()
 	{
+		// Disable kill feed display entirely
 		m_iKillFeedType = EKillFeedType.DISABLED;
 	}
 }
