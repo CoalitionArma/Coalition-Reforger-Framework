bb11ed812abffe0336a834c40801c79bcdd43df2
Troy <tanaka_s2@yahoo.com>
2025-04-28 15:30:28 -0500

    Too many tab

--- scripts/Game/Systems/VanillaOverrides/UI/CRF_SCR_NotificationsComponent.c
+++ scripts/Game/Systems/VanillaOverrides/UI/CRF_SCR_NotificationsComponent.c
@@ -5,6 +5,6 @@ modded class SCR_NotificationsComponent
 	//----------------------------------------------------------------
 	override static bool SendLocalUnlimitedEditor(ENotification notificationID, vector position, int param1 = 0, int param2 = 0, int param3 = 0, int param4 = 0, int param5 = 0, int param6 = 0)
 	{
-			return false;
+		return false;
 	}
 }
