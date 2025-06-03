09feaf0a324a212dc8e7e4fdd3fdae3b8a6cbcc2
Troy <tanaka_s2@yahoo.com>
2025-06-02 19:28:00 -0500

    Calllaters

--- scripts/Game/Systems/Core/Managers/CRF_PlayerControllerManager.c
+++ scripts/Game/Systems/Core/Managers/CRF_PlayerControllerManager.c
@@ -82,10 +82,15 @@ class CRF_PlayerControllerManager : ScriptComponent
 		GetGame().GetInputManager().AddActionListener("SwitchSpectatorUI", EActionTrigger.DOWN, UpdateHUDVisible);
 		
 		// Schedule delayed initialization
-		GetGame().GetCallqueue().CallLater(AddMsgAction, 1000, false);
-		GetGame().GetCallqueue().CallLater(InitFPSLock, 100, false);
-		GetGame().GetCallqueue().CallLater(InitAudioLock, 100, false);
-		GetGame().GetCallqueue().CallLater(OpenCurrentStateMenu, 500, false);
+		//GetGame().GetCallqueue().CallLater(AddMsgAction, 1000, false);
+		//GetGame().GetCallqueue().CallLater(InitFPSLock, 100, false);
+		//GetGame().GetCallqueue().CallLater(InitAudioLock, 100, false);
+		//GetGame().GetCallqueue().CallLater(OpenCurrentStateMenu, 500, false);
+		// Death to calllaters. Readd if non-functional.
+		AddMsgAction();
+		InitFPSLock();
+		InitAudioLock();
+		OpenCurrentStateMenu();
 	}
 
 	/**
