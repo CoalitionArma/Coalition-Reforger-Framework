92cf0e9e94f2e2e3b887fe7e72509d1e93146482
Salamiboio <100155238+Salamiboio@users.noreply.github.com>
2025-09-02 21:27:35 +0300

    Added readme and update for latest CVON

--- scripts/Game/Systems/ModdedOverrides/CVON/CRF_CVON_GroupsManagerComponent.c
+++ scripts/Game/Systems/ModdedOverrides/CVON/CRF_CVON_GroupsManagerComponent.c
@@ -18,6 +18,25 @@ modded class SCR_GroupsManagerComponent
 		if (!playerController)
 			return;
 		
+		if (playerController.m_aRadioSettings.Count() > 0)
+		{
+			foreach (IEntity radio: playerController.m_aRadios)
+			{
+				CVON_RadioComponent radioComp = CVON_RadioComponent.Cast(radio.FindComponent(CVON_RadioComponent));
+				CVON_RadioSettingObject radioSetting = playerController.m_aRadioSettings.Get(playerController.m_aRadios.Find(radio));
+				
+				if (radioComp.m_aChannels.Contains(radioSetting.m_sFreq))
+						radioComp.UpdateChannelServer(radioComp.m_aChannels.Find(radioSetting.m_sFreq) + 1);
+					else
+						radioComp.UpdateChannelServer(radioComp.m_aChannels.Count() + 1);
+				
+				radioComp.UpdateFrequncyServer(radioSetting.m_sFreq);
+				playerController.SetVolumeFromServer(radioSetting.m_iVolume, playerController.m_aRadios.Find(radio));
+				playerController.SetStereoFromServer(radioSetting.m_Stereo, playerController.m_aRadios.Find(radio));
+			}
+			return;
+		}
+		
 		SCR_FactionManager factionMan = SCR_FactionManager.Cast(GetGame().GetFactionManager());
 		SCR_Faction playerFaction = SCR_Faction.Cast(factionMan.GetPlayerFaction(playerId));
 		
