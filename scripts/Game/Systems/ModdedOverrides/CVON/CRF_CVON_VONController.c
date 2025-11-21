7356efc7a90cb29d08f48f9cb0e46f73f73a8415
Salamiboio <100155238+Salamiboio@users.noreply.github.com>
2025-11-20 22:09:11 -0500

    Optimize write json

--- scripts/Game/Systems/ModdedOverrides/CVON/CRF_CVON_VONController.c
+++ scripts/Game/Systems/ModdedOverrides/CVON/CRF_CVON_VONController.c
@@ -370,7 +370,13 @@ modded class SCR_VONController
 			m_PlayerController.BroadcastLocalVONToServer(m_CurrentVONContainer, m_PlayerController.GetPlayerId(), m_CurrentVONContainer.m_iRadioId);
 					
 		}
-		WriteJSON();
+		//Our plugin only checks every 50ms
+		if (m_fVONSaveBuffer >= 0.05)
+		{
+			WriteJSON();
+			m_fVONSaveBuffer = 0;
+		}
+		else m_fVONSaveBuffer += timeSlice;
 	}
 	
 	override void ComputeStereoLR(
