f9937ed2cb945f307b1171fe93a9436b25987552
Salamiboio <100155238+Salamiboio@users.noreply.github.com>
2026-01-27 15:52:48 -0500

    Removes Prints

--- scripts/Game/Systems/ModdedOverrides/Parachute/CRF_ParachuteComponent.c
+++ scripts/Game/Systems/ModdedOverrides/Parachute/CRF_ParachuteComponent.c
@@ -38,20 +38,14 @@ modded class ParachuteComponent
 	
 	override protected bool MayDeployParachute_Internal(IEntity pilot, ParachuteItemComponent item)
 	{
-		Print(pilot);
-		Print(item);
 		if (!pilot || !item)
 			return false;
 
-		Print(m_bParachuteDeployed);
 		if (m_bParachuteDeployed)
 			return false;
 
-		Print(item.GetParachuteUsed());
 		if (item.GetParachuteUsed())
 			return false;
-
-		Print(SCR_ChimeraCharacter.Cast(pilot));
 		SCR_ChimeraCharacter pawn = SCR_ChimeraCharacter.Cast(pilot);
 		if (!pawn)
 			return false;
@@ -64,8 +58,6 @@ modded class ParachuteComponent
 
 		float terrainY = SCR_TerrainHelper.GetTerrainY(pawn.GetOrigin(), null, true);
 		float heightAGL = pawn.GetOrigin()[1] - terrainY;
-		Print(heightAGL);
-		Print(terrainY);
 		if (heightAGL < m_fMinimumAltitude)
 			return false;
 
