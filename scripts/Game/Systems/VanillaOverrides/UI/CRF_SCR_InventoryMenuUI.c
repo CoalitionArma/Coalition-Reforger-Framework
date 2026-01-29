d82fba6cde8ba441137fc763478583b115e6a471
Salamiboio <100155238+Salamiboio@users.noreply.github.com>
2026-01-28 20:39:54 -0500

    Final

--- scripts/Game/Systems/VanillaOverrides/UI/CRF_SCR_InventoryMenuUI.c
+++ scripts/Game/Systems/VanillaOverrides/UI/CRF_SCR_InventoryMenuUI.c
@@ -10,4 +10,61 @@ modded class SCR_InventoryMenuUI
 		}
 		super.OnMenuOpen();
 	}
+	
+	//------------------------------------------------------------------------------------------------
+	override protected void OpenAsNewContainer()
+	{
+		
+		m_pSelectedSlotUI = m_pFocusedSlotUI;
+		auto storage = m_pFocusedSlotUI.GetAsStorage();
+		
+		if (storage)
+		{
+			//Checks if Leader only arsenal is enabled, and if it is prevents non leaders from opening an arsenal
+			CRF_SlottingManager slottingMan = CRF_SlottingManager.GetInstance();
+			if (slottingMan)
+			{
+				SCR_BaseGameMode gamemode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
+				if (storage.GetOwner().FindComponent(SCR_ArsenalComponent) && gamemode.IsLeadersArsenalEnabled())
+				{
+					
+					int playerId = SCR_PlayerController.GetLocalPlayerId();
+					CRF_SlotDataContainer slotData = slottingMan.GetPlayerSlotData(playerId);
+					CRF_ESlotType slotType = slotData.GetSlotType();
+					if (slotType != CRF_ESlotType.TEAM_LEADER && slotType != CRF_ESlotType.SQUAD_LEADER && slotType != CRF_ESlotType.MEDIC)
+						return;
+				}
+			}
+			OpenStorageAsContainer(storage);
+			OpenLinkedStorages(storage);
+			SCR_UISoundEntity.SoundEvent(SCR_SoundEvent.SOUND_INV_HOTKEY_OPEN);
+		}
+	}
+	//------------------------------------------------------------------------------------------------
+	//!
+	override protected void Action_UnfoldItem()
+	{
+		m_pSelectedSlotUI = m_pFocusedSlotUI;
+		auto storage = m_pFocusedSlotUI.GetAsStorage();
+		
+		if (storage)
+		{
+			//Checks if Leader only arsenal is enabled, and if it is prevents non leaders from opening an arsenal
+			CRF_SlottingManager slottingMan = CRF_SlottingManager.GetInstance();
+			if (slottingMan)
+			{
+				SCR_BaseGameMode gamemode = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
+				if (storage.GetOwner().FindComponent(SCR_ArsenalComponent) && gamemode.IsLeadersArsenalEnabled())
+				{
+					
+					int playerId = SCR_PlayerController.GetLocalPlayerId();
+					CRF_SlotDataContainer slotData = slottingMan.GetPlayerSlotData(playerId);
+					CRF_ESlotType slotType = slotData.GetSlotType();
+					if (slotType != CRF_ESlotType.TEAM_LEADER && slotType != CRF_ESlotType.SQUAD_LEADER && slotType != CRF_ESlotType.MEDIC)
+						return;
+				}
+			}
+		}
+ 		SimpleFSM( EMenuAction.ACTION_UNFOLD );
+	}
 }
