487a89e3bd80437d49e4c804689b683c94e171fd
Njpatman <77807058+Njpatman@users.noreply.github.com>
2026-03-07 09:58:18 -0500

    Move GS System To ChimeraSystems

--- Scripts/Game/Systems/Core/Managers/Gamemode/Gearscript/CRF_VehicleGearscriptManager.c
+++ Scripts/Game/Systems/Core/Managers/Gamemode/Gearscript/CRF_VehicleGearscriptManager.c
@@ -438,7 +438,7 @@ class CRF_VehicleGearscriptManager : ScriptComponent
 		if (!truck)
 			return;
 		
-		ref CRF_GearScriptConfig gearSriptConfig = CRF_GearscriptManager.GetInstance().LoadGearScriptConfig(gsContainer.m_rGearScript);
+		ref CRF_GearScriptConfig gearSriptConfig = CRF_GearscriptSystem.GetInstance().LoadGearScriptConfig(gsContainer.m_rGearScript);
 		ref CRF_VehicleGearscriptConfig vehicleGearScriptConfig = LoadVehicleGearScriptConfig(gsContainer.m_rVehicleGearscriptValues);
 		SCR_VehicleInventoryStorageManagerComponent invManager = SCR_VehicleInventoryStorageManagerComponent.Cast(truck.FindComponent(SCR_VehicleInventoryStorageManagerComponent));
 		if (!invManager)
