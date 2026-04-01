32dd65aa39f315851464bba21a81ba1793406e6f
MrPvTDagger <mrpvtdagger@gmail.com>
2026-04-01 19:17:17 -0400

    delete old rallypoint

--- Scripts/Game/Systems/Core/Entities/SpawnPoints/CRF_RallyPoint.c
+++ Scripts/Game/Systems/Core/Entities/SpawnPoints/CRF_RallyPoint.c
@@ -7,6 +7,9 @@ class CRF_RallyPoint: GenericEntity
 	ref CRF_SpawnPointData m_SpawnPointSettings = new CRF_SpawnPointData();
 	
 	protected int m_iLocallyStoredId;
+	
+	protected SCR_AIGroup m_group;
+	protected Faction m_faction;
 
 	//------------------------------------------------------------------------------------------------
 	void SetupRallyPoint()
@@ -23,17 +26,23 @@ class CRF_RallyPoint: GenericEntity
 			return;
 		
 		int playerId = compositionComponent.GetBuilderId();
+		if (playerId == -1)
+			return;
 
-		SCR_AIGroup group = slottingManager.GetPlayerSlotGroup(playerId);
-		if (!group)
+		m_group = slottingManager.GetPlayerSlotGroup(playerId);
+		if (!m_group)
 			return;
 		
-		Faction faction = slottingManager.GetPlayerSlotFaction(playerId);
+		m_faction = slottingManager.GetPlayerSlotFaction(playerId);
+		if (!m_faction)
+			return;
 		
-		string groupCustomName = group.GetCustomNameWithOriginal();
+		string groupCustomName = m_group.GetCustomNameWithOriginal();
+		
+		RemovePreviousRallyPoint();
 		
 		m_SpawnPointSettings.SetSpawnPointName(groupCustomName + " RP");
-		m_SpawnPointSettings.SetSpawnPointFaction(factions.Find(faction.GetFactionKey()));
+		m_SpawnPointSettings.SetSpawnPointFaction(factions.Find(m_faction.GetFactionKey()));
 		m_SpawnPointSettings.SetRestrictedToGroup(groupCustomName);
 		m_SpawnPointSettings.SetSpawnPointActive(true);
 		
@@ -51,6 +60,26 @@ class CRF_RallyPoint: GenericEntity
 	void SetLocalSpawnPointId(int spawnPointId)
 	{
 		m_iLocallyStoredId = spawnPointId;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	private void RemovePreviousRallyPoint()
+	{
+		array<CRF_SpawnPointData> factionRespawnPoints = CRF_RespawnManager.GetInstance().GetFactionSpawnpoints(m_faction.GetFactionKey(), m_group);
+		
+		foreach(CRF_SpawnPointData spawnPointData : factionRespawnPoints)
+		{ 
+			IEntity entity = CRF_EntityHelper.GetEntityFromRplId(spawnPointData.GetSpawnPointEntity());
+			if (!entity)
+				continue;
+			
+			CRF_RallyPoint rally = CRF_RallyPoint.Cast(entity);
+			if (!rally)
+				continue;
+			
+			if (rally.m_group == m_group)			
+				SCR_EntityHelper.DeleteEntityAndChildren(entity);
+		}
 	}
 	
 	//------------------------------------------------------------------------------------------------
