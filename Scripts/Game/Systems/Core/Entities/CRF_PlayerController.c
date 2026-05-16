d6208e48c349ccaa777de3e464fadfa199610192
Troy <tanaka_s2@yahoo.com>
2026-05-16 00:07:21 -0500

    NPE fix

--- Scripts/Game/Systems/Core/Entities/CRF_PlayerController.c
+++ Scripts/Game/Systems/Core/Entities/CRF_PlayerController.c
@@ -38,11 +38,15 @@ class CRF_PlayerController : SCR_PlayerController
 		if (from)
 		{
 			SCR_CharacterControllerComponent charController = SCR_CharacterControllerComponent.Cast(from.FindComponent(SCR_CharacterControllerComponent));
-			if (charController.IsDead())
+			if (charController && charController.IsDead())
 			{
-				vector mat[4];
-				from.GetTransform(mat);
-				CRF_PlayerControllerManager.GetInstance().m_vPlayersLastDeath = mat;
+				CRF_PlayerControllerManager pcm = CRF_PlayerControllerManager.GetInstance();
+				if (pcm)
+				{
+					vector mat[4];
+					from.GetTransform(mat);
+					pcm.m_vPlayersLastDeath = mat;
+				}
 			};
 		}
 		
