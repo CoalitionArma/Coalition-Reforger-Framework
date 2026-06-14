be4d09b0be6a4a570220a2469b3cfa46dccdbf0f
Troy <tanaka_s2@yahoo.com>
2026-06-13 19:56:41 -0500

    Change to any friendly within 100m

--- Scripts/Game/Systems/Core/Entities/Characters/CRF_PlayerCharacter.c
+++ Scripts/Game/Systems/Core/Entities/Characters/CRF_PlayerCharacter.c
@@ -131,6 +131,12 @@ class CRF_PlayerCharacter : SCR_ChimeraCharacter
 		FactionKey myFaction = CRF_EntityHelper.DetermineFactionKey(this);
 		float maxDistSq = Math.Pow(Math.Max(m_fFriendlyProximityMeters, 1.0), 2);
 
+		// Proximity is based on any friendly player, not restricted to the player's group.
+		return !HasFriendlyWithinRange(playerManager, playerId, myFaction, maxDistSq);
+	}
+
+	protected bool HasFriendlyWithinRange(PlayerManager playerManager, int playerId, FactionKey myFaction, float maxDistSq)
+	{
 		array<int> allPlayers = {};
 		playerManager.GetPlayers(allPlayers);
 
@@ -154,10 +160,10 @@ class CRF_PlayerCharacter : SCR_ChimeraCharacter
 				continue;
 
 			if (vector.DistanceSq(GetOrigin(), otherEntity.GetOrigin()) <= maxDistSq)
-				return false;
+				return true;
 		}
 
-		return true;
+		return false;
 	}
 
 	protected void UpdatePenaltyState(float targetPenalty, float timeSlice)
