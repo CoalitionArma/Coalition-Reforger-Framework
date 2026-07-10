08e940a6417b11244f9f04fb94cb01a46d2f65b0
TristGit <trist234@yahoo.com>
2026-07-09 21:47:42 -0700

    Initial (#1528)
    Includes component for mission, actions, and layer edits.
    
    Co-authored-by: Troy <tanaka_s2@yahoo.com>

---
+++ Scripts/Game/Systems/Components/RadioExtraction/CRF_RadioExtractionPlantAction.c
@@ -0,0 +1,83 @@
+//------------------------------------------------------------------------------------
+// CRF_RadioExtractionPlantAction: Plant action for the Radio Extraction lobby prop
+// Allows the Planter side to arm the bomb, starting its detonation countdown
+//------------------------------------------------------------------------------------
+
+class CRF_RadioExtractionPlantAction : ScriptedUserAction
+{
+	protected CRF_RadioExtraction m_RadioExtraction;
+
+	//------------------------------------------------------------------------------------------------
+	override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
+	{
+		if (!GetGame().InPlayMode())
+			return;
+
+		m_RadioExtraction = CRF_RadioExtraction.GetInstance();
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+		if (!pOwnerEntity || !pUserEntity || !m_RadioExtraction)
+			return;
+
+		ChimeraCharacter character = ChimeraCharacter.Cast(pUserEntity);
+		if (!character)
+			return;
+
+		// Stop the planting sound since the action completed, then arm the bomb
+		CRF_PlayerRplToAuthorityManager.GetInstance().RequestStopPositionalSound(m_RadioExtraction.m_sPlantSoundEvent);
+		CRF_PlayerRplToAuthorityManager.GetInstance().RequestRadioExtractionSetPlanted(true);
+
+		super.PerformAction(pOwnerEntity, pUserEntity);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void OnActionStart(IEntity pUserEntity)
+	{
+		super.OnActionStart(pUserEntity);
+
+		if (m_RadioExtraction && GetOwner())
+			CRF_PlayerRplToAuthorityManager.GetInstance().RequestPlayPositionalSound(m_RadioExtraction.m_rPlantSoundResource, m_RadioExtraction.m_sPlantSoundEvent, GetOwner().GetOrigin());
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void OnActionCanceled(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+		super.OnActionCanceled(pOwnerEntity, pUserEntity);
+
+		if (m_RadioExtraction)
+			CRF_PlayerRplToAuthorityManager.GetInstance().RequestStopPositionalSound(m_RadioExtraction.m_sPlantSoundEvent);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool CanBeShownScript(IEntity user)
+	{
+		if (!m_RadioExtraction || !user)
+			return false;
+
+		return m_RadioExtraction.IsUserOnSide(user, CRF_ERadioExtractionSide.PLANTER);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool CanBePerformedScript(IEntity user)
+	{
+		if (!m_RadioExtraction)
+			return false;
+
+		return !m_RadioExtraction.IsBombPlanted() && !m_RadioExtraction.IsDetonated();
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool HasLocalEffectOnlyScript()
+	{
+		return true;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override bool CanBroadcastScript()
+	{
+		return false;
+	}
+}
